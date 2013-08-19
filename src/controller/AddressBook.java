c03412b96888ea04ae45af06228f1d457acc9e6a
Marc-Antoine Perennou <Marc-Antoin@Perennou.com>
2013-08-19 16:21:14 +0200

    update controller

--- src/controller/AddressBook.java
+++ src/controller/AddressBook.java
@@ -2,12 +2,13 @@ package controller;
 
 import modele.Personne;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface AddressBook {
 
-    List<Personne> getPeople();
-    void addPerson(String nom, String prenom, String telephone, String dateNaissance);
-	void removePerson(String parameter);
-	void modifierPerson(String parameter, Personne per);
+    Collection<Personne> getPeople();
+    String addPerson(String nom, String prenom, String telephone, String dateNaissance);
+	void removePerson(String id);
+	void modifierPerson(String id, Personne person);
 }
