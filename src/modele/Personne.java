c43205b25edcf265a2b1360bc3c05d238d3efcac
Mickael FONTAINE <fontaine.mickael@ymail.com>
2013-08-19 14:58:28 +0200

    Ajout src

---
+++ src/modele/Personne.java
@@ -0,0 +1,33 @@
+package modele;
+
+public class Personne {
+	private String nom;
+	private String prenom;
+	private String telephone;
+	private String dateNaissance;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+}
