1aa6d898592c809f6119c653fcb527ec0be736fc
Mickael FONTAINE <fontaine.mickael@ymail.com>
2013-08-21 14:33:25 +0200

    Essai de fail

---
+++ test/org/imie/PersonneTest.java
@@ -0,0 +1,42 @@
+package org.imie;
+
+import static org.junit.Assert.*;
+
+import modele.Personne;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.*;
+
+
+public class PersonneTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetNom() {
+		Personne per = new Personne(null, null, null, null);
+		per.setNom("bobo");
+		assertEquals(per.getNom(),"bobo");
+		fail("ça doit aps marcher");
+		
+	}
+
+}
