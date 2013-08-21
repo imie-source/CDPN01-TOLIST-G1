6ea8282d156f0d0876d4e47717cb5d95e1bfefb7
imie <imie@imie-VirtualBox.(none)>
2013-08-21 12:14:41 +0200

    ajout de 2 tests

--- test/org/imie/Sample.java
+++ test/org/imie/Sample.java
@@ -2,18 +2,44 @@ package org.imie;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import modele.Personne;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import controller.IMIEAddressBook;
 
 public class Sample {
 
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void emptyPeople() {
+		// Asset
+		IMIEAddressBook book = new IMIEAddressBook(); 
+		List<Personne> collection = new ArrayList<Personne>();
+		
+		// Act
+		
+		// Assert
+		assertArrayEquals(collection.toArray(), book.getPeople().toArray());
 	}
 	
 	@Test
-	public void test2() {
-		//fail("Not yet implemented");
+	public void addPerson() {
+		// Asset
+		IMIEAddressBook book = new IMIEAddressBook(); 
+		List<Personne> collection = new ArrayList<Personne>();
+		collection.add(new Personne("L'éponge", "Bob", "06.23.23.23.23", "21/08/2013"));
+		
+		// Act
+		book.addPerson("L'éponge", "Bob", "06.23.23.23.23", "21/08/2013");
+		
+		// Assert
+		assertEquals(collection.get(0).getNom(), ((List<Personne>) book.getPeople()).get(0).getNom());
 	}
 
 }
